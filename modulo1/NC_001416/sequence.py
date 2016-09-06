f7495084414c8c88449d21ac27774dc5ab6d8c9a
Anna Paula Maule <Anna Paula Maule>
2016-09-05 22:41:03 -0300

    genarated more charts and a report

--- modulo1/NC_001416/sequence.py
+++ modulo1/NC_001416/sequence.py
@@ -6,6 +6,11 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.tools as tls
 
+username = input("Plotly username: ")
+apiKey = input("Plotly API Key: ")
+
+tls.set_credentials_file(username, apiKey)
+
 bacteria=[]
 bacteriaG=[]
 for seq_record in SeqIO.parse("sequence.fasta","fasta"):
@@ -13,30 +18,30 @@ for seq_record in SeqIO.parse("sequence.fasta","fasta"):
     print(repr(seq_record.seq))
     print(len(seq_record))
 
-    bacteria.append(seq_record[1:100].seq.count('C'))
+
     bacteria.append(seq_record[1:500].seq.count('C'))
-    bacteria.append(seq_record[500:1100].seq.count('C'))
-    bacteria.append(seq_record[1100:1800].seq.count('C'))
-    bacteria.append(seq_record[1800:2600].seq.count('C'))
-    bacteria.append(seq_record[2600:3500].seq.count('C'))
-    bacteria.append(seq_record[3500:4500].seq.count('C'))
-    bacteria.append(seq_record[4500:5600].seq.count('C'))
-    bacteria.append(seq_record[5600:6800].seq.count('C'))
-    bacteria.append(seq_record[6800:7100].seq.count('C'))
-    bacteria.append(seq_record[7100:8500].seq.count('C'))
+    bacteria.append(seq_record[1:1100].seq.count('C'))
+    bacteria.append(seq_record[1:1800].seq.count('C'))
+    bacteria.append(seq_record[1:2600].seq.count('C'))
+    bacteria.append(seq_record[1:3500].seq.count('C'))
+    bacteria.append(seq_record[1:4500].seq.count('C'))
+    bacteria.append(seq_record[1:5600].seq.count('C'))
+    bacteria.append(seq_record[1:6800].seq.count('C'))
+    bacteria.append(seq_record[1:7100].seq.count('C'))
+    bacteria.append(seq_record[1:8500].seq.count('C'))
 
 
 
     bacteriaG.append(seq_record[1:500].seq.count('G'))
-    bacteriaG.append(seq_record[500:1100].seq.count('G'))
-    bacteriaG.append(seq_record[1100:1800].seq.count('G'))
-    bacteriaG.append(seq_record[1800:2600].seq.count('G'))
-    bacteriaG.append(seq_record[2600:3500].seq.count('G'))
-    bacteriaG.append(seq_record[3500:4700].seq.count('G'))
-    bacteriaG.append(seq_record[4500:5600].seq.count('G'))
-    bacteriaG.append(seq_record[5600:6800].seq.count('G'))
-    bacteriaG.append(seq_record[6800:7100].seq.count('G'))
-    bacteriaG.append(seq_record[7100:8500].seq.count('G'))
+    bacteriaG.append(seq_record[1:1100].seq.count('G'))
+    bacteriaG.append(seq_record[1:1800].seq.count('G'))
+    bacteriaG.append(seq_record[1:2600].seq.count('G'))
+    bacteriaG.append(seq_record[1:3500].seq.count('G'))
+    bacteriaG.append(seq_record[1:4700].seq.count('G'))
+    bacteriaG.append(seq_record[1:5600].seq.count('G'))
+    bacteriaG.append(seq_record[1:6800].seq.count('G'))
+    bacteriaG.append(seq_record[1:7100].seq.count('G'))
+    bacteriaG.append(seq_record[1:8500].seq.count('G'))
 
 
 bacteriaCG=[]
@@ -46,5 +51,5 @@ for i in range(0,10):
 trace1=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteria[0],bacteria[1],bacteria[2],bacteria[3],bacteria[4],bacteria[5],bacteria[6],bacteria[7],bacteria[8],bacteria[9]])
 trace2=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteriaG[0],bacteriaG[1],bacteriaG[2],bacteriaG[3],bacteriaG[4],bacteriaG[5],bacteriaG[6],bacteriaG[7],bacteriaG[8],bacteriaG[9]])
 trace3=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteriaCG[0],bacteriaCG[1],bacteriaCG[2],bacteriaCG[3],bacteriaCG[4],bacteriaCG[5],bacteriaCG[6],bacteriaCG[7],bacteriaCG[8],bacteriaCG[9]])
-data=[trace1,trace2]
+data=[trace1,trace2,trace3]
 py.plot(data, filename="BacteriaCGcount")
