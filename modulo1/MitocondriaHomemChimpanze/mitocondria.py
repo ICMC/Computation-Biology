09212b19cb6e537bbc2edddc3b6068e0255ba207
Anna Paula Maule <Anna Paula Maule>
2016-09-21 22:15:13 -0300

    deleted files that had incorrect information and added the correct report

--- modulo1/MitocondriaHomemChimpanze/mitocondria.py
+++ modulo1/MitocondriaHomemChimpanze/mitocondria.py
@@ -6,44 +6,55 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.tools as tls
 
-username = input("Plotly username: ")
-apiKey = input("Plotly API Key: ")
+#username = input("Plotly username: ")
+#apiKey = input("Plotly API Key: ")
+
+username= "pawpepe"
+apiKey = "07lxwcy4d2"
+tls.set_credentials_file(username, apiKey)
 
 tls.set_credentials_file(username, apiKey)
-homo = []
-homoG=[]
-chimp=[]
-chimpG=[]
+homo1 = []
+homo2 = []
+homo3 = []
+chimp1 = []
+chimp2 = []
+chimp3 = []
+
+
+windowSize1 = int(input("Window Size1: "))
+windowSize2 = int(input("Window Size2: "))
+windowSize3 = int(input("Window Size3: "))
+
 
 for seq_record in SeqIO.parse("homoSapiensMito.fasta","fasta"):
     print(seq_record.id)
     print(repr(seq_record.seq))
     print(len(seq_record))
 
-    homo.append(seq_record[1:100].seq.count('C'))
-
-    homo.append(seq_record[1:500].seq.count('C'))
-    homo.append(seq_record[500:1100].seq.count('C'))
-    homo.append(seq_record[1100:1800].seq.count('C'))
-    homo.append(seq_record[1800:2600].seq.count('C'))
-    homo.append(seq_record[2600:3500].seq.count('C'))
-    homo.append(seq_record[3500:4500].seq.count('C'))
-    homo.append(seq_record[4500:5600].seq.count('C'))
-    homo.append(seq_record[5600:6800].seq.count('C'))
-    homo.append(seq_record[6800:7100].seq.count('C'))
-    homo.append(seq_record[7100:8500].seq.count('C'))
 
 
-    homoG.append(seq_record[1:500].seq.count('G'))
-    homoG.append(seq_record[500:1100].seq.count('G'))
-    homoG.append(seq_record[1100:1800].seq.count('G'))
-    homoG.append(seq_record[1800:2600].seq.count('G'))
-    homoG.append(seq_record[2600:3500].seq.count('G'))
-    homoG.append(seq_record[3500:4500].seq.count('G'))
-    homoG.append(seq_record[4500:5600].seq.count('G'))
-    homoG.append(seq_record[5600:6800].seq.count('G'))
-    homoG.append(seq_record[6800:7100].seq.count('G'))
-    homoG.append(seq_record[7100:8500].seq.count('G'))
+    x1 = int(windowSize1)
+    x2 = int(windowSize2)
+    x3 = int(windowSize3)
+    x=1
+
+
+    for y in range(0,8):
+      homo1.append(seq_record[x:x1].seq.count('C') + seq_record[x:x1].seq.count('G'))
+      x = x1
+      x1 += windowSize1
+
+    x=1
+    for y in range(0,8):
+      homo2.append(seq_record[x:x2].seq.count('C') + seq_record[x:x2].seq.count('G'))
+      x = x2
+      x2 += windowSize2
+    x=1
+    for y in range(0,8):
+      homo3.append(seq_record[x:x3].seq.count('C') + seq_record[x:x3].seq.count('G'))
+      x = x3
+      x3 += windowSize3
 
 
 for seq_record in SeqIO.parse("pantroglodytes.fasta","fasta"):
@@ -51,41 +62,43 @@ for seq_record in SeqIO.parse("pantroglodytes.fasta","fasta"):
     print(repr(seq_record.seq))
     print(len(seq_record))
 
-    chimp.append(seq_record[1:100].seq.count('C'))
-    chimp.append(seq_record[1:500].seq.count('C'))
-    chimp.append(seq_record[500:1100].seq.count('C'))
-    chimp.append(seq_record[1100:1800].seq.count('C'))
-    chimp.append(seq_record[1800:2600].seq.count('C'))
-    chimp.append(seq_record[2600:3500].seq.count('C'))
-    chimp.append(seq_record[3500:4500].seq.count('C'))
-    chimp.append(seq_record[4500:5600].seq.count('C'))
-    chimp.append(seq_record[5600:6800].seq.count('C'))
-    chimp.append(seq_record[6800:7100].seq.count('C'))
-    chimp.append(seq_record[7100:8500].seq.count('C'))
+    x1 = int(windowSize1)
+    x2 = int(windowSize2)
+    x3 = int(windowSize3)
+    x=1
+
+    for y in range(0,8):
+      chimp1.append(seq_record[x:x1].seq.count('C') + seq_record[x:x1].seq.count('G'))
+      x = x1
+      x1 += windowSize1
+
+    x=1
+    for y in range(0,8):
+      chimp2.append(seq_record[x:x2].seq.count('C') + seq_record[x:x2].seq.count('G'))
+      x = x2
+      x2 += windowSize2
+    x=1
+    for y in range(0,8):
+      chimp3.append(seq_record[x:x3].seq.count('C') + seq_record[x:x3].seq.count('G'))
+      x = x3
+      x3 += windowSize3
+
+trace1=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[homo1[0],homo1[1],homo1[2],homo1[3],homo1[4],homo1[5],homo1[6],homo1[7]])
+trace2=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[chimp1[0],chimp1[1],chimp1[2],chimp1[3],chimp1[4],chimp1[5],chimp1[6],chimp1[7]])
+data=[trace1,trace2]
+py.plot(data, filename="genes 1")
 
 
-
-    chimpG.append(seq_record[1:500].seq.count('G'))
-    chimpG.append(seq_record[500:1100].seq.count('G'))
-    chimpG.append(seq_record[1100:1800].seq.count('G'))
-    chimpG.append(seq_record[1800:2600].seq.count('G'))
-    chimpG.append(seq_record[2600:3500].seq.count('G'))
-    chimpG.append(seq_record[3500:4700].seq.count('G'))
-    chimpG.append(seq_record[4500:5600].seq.count('G'))
-    chimpG.append(seq_record[5600:6800].seq.count('G'))
-    chimpG.append(seq_record[6800:7100].seq.count('G'))
-    chimpG.append(seq_record[7100:8500].seq.count('G'))
-
-
-homoCG=[]
-chimpCG=[]
-for i in range(0,10):
-    homoCG.append(homo[i]+homoG[i])
-    chimpCG.append(chimp[i]+chimpG[i])
-
-
-trace1=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[homoCG[0],homoCG[1],homoCG[2],homoCG[3],homoCG[4],homoCG[5],homoCG[6],homoCG[7],homoCG[8],homoCG[9]])
-trace2=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[chimpCG[0],chimpCG[1],chimpCG[2],chimpCG[3],chimpCG[4],chimpCG[5],chimpCG[6],chimpCG[7],chimpCG[8],chimpCG[9]])
+trace1=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[homo2[0],homo2[1],homo2[2],homo2[3],homo2[4],homo2[5],homo2[6],homo2[7]])
+trace2=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[chimp2[0],chimp2[1],chimp2[2],chimp2[3],chimp2[4],chimp2[5],chimp2[6],chimp2[7]])
 
 data=[trace1,trace2]
-py.plot(data, filename="genes")
+py.plot(data, filename="genes 2")
+
+
+
+trace1=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[homo3[0],homo3[1],homo3[2],homo3[3],homo3[4],homo3[5],homo3[6],homo3[7]])
+trace2=go.Scatter(x=[1,2,3,4,5,6,7,8], y=[chimp3[0],chimp3[1],chimp3[2],chimp3[3],chimp3[4],chimp3[5],chimp3[6],chimp3[7]])
+
+data=[trace1,trace2]
+py.plot(data, filename="genes 3")
--- modulo1/NC_001416/sequence.py
+++ modulo1/NC_001416/sequence.py
@@ -6,50 +6,54 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.tools as tls
 
-username = input("Plotly username: ")
-apiKey = input("Plotly API Key: ")
+#username = input("Plotly username: ")
+#apiKey = input("Plotly API Key: ")
 
+username= "pawpepe"
+apiKey = "07lxwcy4d2"
 tls.set_credentials_file(username, apiKey)
 
-bacteria=[]
-bacteriaG=[]
+bacteria1=[]
+bacteria2=[]
+bacteria3=[]
+
 for seq_record in SeqIO.parse("sequence.fasta","fasta"):
     print(seq_record.id)
     print(repr(seq_record.seq))
     print(len(seq_record))
 
-
-    bacteria.append(seq_record[1:500].seq.count('C'))
-    bacteria.append(seq_record[1:1100].seq.count('C'))
-    bacteria.append(seq_record[1:1800].seq.count('C'))
-    bacteria.append(seq_record[1:2600].seq.count('C'))
-    bacteria.append(seq_record[1:3500].seq.count('C'))
-    bacteria.append(seq_record[1:4500].seq.count('C'))
-    bacteria.append(seq_record[1:5600].seq.count('C'))
-    bacteria.append(seq_record[1:6800].seq.count('C'))
-    bacteria.append(seq_record[1:7100].seq.count('C'))
-    bacteria.append(seq_record[1:8500].seq.count('C'))
+    maxLen= 48502
 
 
 
-    bacteriaG.append(seq_record[1:500].seq.count('G'))
-    bacteriaG.append(seq_record[1:1100].seq.count('G'))
-    bacteriaG.append(seq_record[1:1800].seq.count('G'))
-    bacteriaG.append(seq_record[1:2600].seq.count('G'))
-    bacteriaG.append(seq_record[1:3500].seq.count('G'))
-    bacteriaG.append(seq_record[1:4700].seq.count('G'))
-    bacteriaG.append(seq_record[1:5600].seq.count('G'))
-    bacteriaG.append(seq_record[1:6800].seq.count('G'))
-    bacteriaG.append(seq_record[1:7100].seq.count('G'))
-    bacteriaG.append(seq_record[1:8500].seq.count('G'))
+    windowSize1 = int(input("Window Size1: "))
+    windowSize2 = int(input("Window Size2: "))
+    windowSize3 = int(input("Window Size3: "))
+
+    x1 = int(windowSize1)
+    x2 = int(windowSize2)
+    x3 = int(windowSize3)
+    x=1
+
+    for y in range(0,8):
+      bacteria1.append(seq_record[x:x1].seq.count('C') + seq_record[x:x1].seq.count('G'))
+      x = x1
+      x1 += windowSize1
+
+    x=1
+    for y in range(0,22):
+      bacteria2.append(seq_record[x:x2].seq.count('C') + seq_record[x:x2].seq.count('G'))
+      x = x2
+      x2 += windowSize2
+    x=1
+    for y in range(0,44):
+      bacteria3.append(seq_record[x:x3].seq.count('C') + seq_record[x:x3].seq.count('G'))
+      x = x3
+      x3 += windowSize3
 
 
-bacteriaCG=[]
-for i in range(0,10):
-    bacteriaCG.append(bacteria[i]+bacteriaG[i])
-
-trace1=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteria[0],bacteria[1],bacteria[2],bacteria[3],bacteria[4],bacteria[5],bacteria[6],bacteria[7],bacteria[8],bacteria[9]])
-trace2=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteriaG[0],bacteriaG[1],bacteriaG[2],bacteriaG[3],bacteriaG[4],bacteriaG[5],bacteriaG[6],bacteriaG[7],bacteriaG[8],bacteriaG[9]])
-trace3=go.Scatter(x=[1,2,3,4,5,6,7,8,9,10], y=[bacteriaCG[0],bacteriaCG[1],bacteriaCG[2],bacteriaCG[3],bacteriaCG[4],bacteriaCG[5],bacteriaCG[6],bacteriaCG[7],bacteriaCG[8],bacteriaCG[9]])
+trace1=go.Scatter(x=[100,200,300,400,500,600,700,800,900], y=[bacteria1[0],bacteria1[1],bacteria1[2],bacteria1[3],bacteria1[4],bacteria1[5],bacteria1[6],bacteria1[7]])
+trace2=go.Scatter(x=[2000,4000,6000,8000,10000,12000,14000,16000], y=[bacteria2[0],bacteria2[1],bacteria2[2],bacteria2[3],bacteria2[4],bacteria2[5],bacteria2[6],bacteria2[7]])
+trace3=go.Scatter(x=[5500,11000,16500,22000,27500,33000,38500,44000], y=[bacteria3[0],bacteria3[1],bacteria3[2],bacteria3[3],bacteria3[4],bacteria3[5],bacteria3[6],bacteria3[7]])
 data=[trace1,trace2,trace3]
-py.plot(data, filename="BacteriaCGcount")
+py.plot(data, filename="bacteriacount")
